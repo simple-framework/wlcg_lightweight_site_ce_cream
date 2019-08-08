1f78648b277c9997e9a3f2900b03e5b424c8a2f4
maany <imptodefeat@gmail.com>
2019-08-08 23:27:38 +0200

    update edgusers

--- yaim/pre_config/categories/edgusers_static.py
+++ yaim/pre_config/categories/edgusers_static.py
@@ -2,7 +2,9 @@ from models.parameter_category import ParameterCategory
 
 def get(data):
     static_category = ParameterCategory("edgusers_static", data)
-    edgusers = "155:${GLITE_USER}:155:${GLITE_GROUP}:gLite user:${GLITE_HOME_DIR} \n" \
+    edgusers = "152:${EDG_USER}:152,151:${EDG_GROUP},${INFOSYS_GROUP}:EDG user:${EDG_HOME_DIR}\n" \
+               "153:${EDGINFO_USER}:153,151:${EDGINFO_USER},${INFOSYS_GROUP}:EDG info user:${EDGINFO_HOME_DIR}\n" \
+               "155:${GLITE_USER}:155:${GLITE_GROUP}:gLite user:${GLITE_HOME_DIR} \n" \
                "154:${BDII_USER}:158:${BDII_GROUP}:BDII user:${BDII_HOME_DIR}"
     static_category.add(edgusers)
     return [static_category]
